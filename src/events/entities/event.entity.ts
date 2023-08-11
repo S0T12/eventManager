9887e7c0e90dfaff926187c726bd161ef391bc9c
s0a1e2 <oops2198@gmail.com>
2023-08-11 10:31:51 +0330

    complete entity event

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,42 @@
-export class Event {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Event {
+  @PrimaryGeneratedColumn({ type: 'number' })
+  id: number;
+
+  @Column({ type: 'string' })
+  title: string;
+
+  @Column({ type: 'string' })
+  description: string;
+
+  @Column({ type: 'string' })
+  location: string;
+
+  @Column({ type: 'number' })
+  categoryId: number;
+
+  @Column({ type: 'date' })
+  eventDate: Date;
+
+  @Column({ type: 'number' })
+  userId: number;
+
+  @Column({ type: 'boolean' })
+  active: boolean;
+
+  @CreateDateColumn({ type: 'date' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'date' })
+  updateAt: Date;
+
+  @DeleteDateColumn({ type: 'date' })
+  deletedAt: Date;
+}
