4cf01320a287108ea0ca4592abe5c10e2260f5ab
s0a1e2 <oops2198@gmail.com>
2023-08-11 14:57:43 +0330

    complete!!

--- src/auth/jwt-guard/jwt-guard.guard.ts
+++ src/auth/jwt-guard/jwt-guard.guard.ts
@@ -22,8 +22,6 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstant.secret,
       });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
