f928bd6a967fa7955fed262b1bd3adba5a73cf40
Shayan-Eskandari <shayanss8080@gmail.com>
2023-08-11 11:41:29 +0330

    event dto is done

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEventDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  categoryId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  active: boolean;
+}
